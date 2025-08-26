e1cab0d73b2585292f22af423f7e04dc5e7474ad
purplerice1212 <super4yl@gmail.com>
2025-08-26 17:06:14 +0100

    Handle zero-size bounds in fitCanvas

--- src/utils/fit.js
+++ src/utils/fit.js
@@ -6,7 +6,11 @@ export function fitCanvas(canvas, bounds) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const width = bounds.max[0] - bounds.min[0];
   const height = bounds.max[1] - bounds.min[1];
-  const scale = Math.min(canvas.width / width, canvas.height / height);
+  const EPSILON = 1e-6;
+  let scale = 1;
+  if (Math.abs(width) > EPSILON && Math.abs(height) > EPSILON) {
+    scale = Math.min(canvas.width / width, canvas.height / height);
+  }
   ctx.setTransform(scale, 0, 0, -scale, -bounds.min[0] * scale, canvas.height + bounds.min[1] * scale);
   return ctx;
 }
