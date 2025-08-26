ed0a7b62f8e1e72f1106061b1969c24778be031e
purplerice1212 <super4yl@gmail.com>
2025-08-26 18:03:56 +0100

    Remove unused makeCamera helper

--- src/utils/camera3d.js
+++ src/utils/camera3d.js
@@ -1,12 +1,3 @@
-export function makeCamera(bounds) {
-  const distance = Math.max(bounds.width, bounds.depth) * 2;
-  return {
-    distance,
-    cx: bounds.width / 2,
-    cy: bounds.height / 2
-  };
-}
-
 export function project(point, camera) {
   const {x, y, z} = point;
   const scale = camera.distance / (camera.distance + z);
