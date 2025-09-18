1d912daac9a92cf8ce93cce23f5767863c7093b3
purplerice1212 <super4yl@gmail.com>
2025-09-18 11:46:59 +0100

    Allow channels to use precomputed segments

--- src/tests/bin-slack.js
+++ src/tests/bin-slack.js
@@ -1,6 +1,7 @@
 import {buildModel} from '../model.js';
 import {getState} from '../state.js';
 import {channels} from '../utils/channels.js';
+import {segments} from '../utils/segments.js';
 import {mm} from '../utils/mm.js';
 
 export function testBinSlack(){
@@ -10,7 +11,8 @@ export function testBinSlack(){
     const S = {...getState(), binSlack:20};
     const M = buildModel(S);
     const bin = M.boxes.find(b=>b.type==='bin');
-    const chWidth = channels(S)[0].w;
+    const seg = segments(S);
+    const chWidth = channels(S, seg)[0].w;
     const expectedBody = Math.min(mm(S.binBody), Math.max(0, chWidth - 2 - mm(S.binSlack)));
     const expectedOverall = expectedBody + 2*mm(S.binFlange) + mm(S.binSlack);
     const pass = Math.abs(bin.w - expectedOverall) < 1e-6;
