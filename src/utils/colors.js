acd3434c1e886ba6dc428f21b477b1166ec97ffe
purplerice1212 <super4yl@gmail.com>
2025-09-25 12:13:04 +0100

    Align timber colours with legend palette

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,8 +1,8 @@
 export const typeColors = {
-  post: '#8d5524',
-  rail: '#b22222',
-  bin: '#1e90ff',
-  support: '#228b22',
-  supportBatten: '#228b22',
-  lintel: '#daa520'
+  post: '#f472b6', // Posts: vivid pink to spotlight the vertical uprights in 3D views.
+  rail: '#60a5fa', // Rails: cool blue to emphasise the horizontal run along the frame.
+  bin: '#f87171', // Bins: punchy red to differentiate removable storage elements.
+  support: '#fb923c', // Supports: bright orange to highlight diagonal or bracing members.
+  supportBatten: '#fb923c', // Support battens: share the support orange to show they reinforce the same assemblies.
+  lintel: '#9ca3af' // Lintels: neutral gray to convey overhead structural spans.
 };
